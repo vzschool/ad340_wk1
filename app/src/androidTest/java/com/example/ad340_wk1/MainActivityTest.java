bd17485a1d5652e10182b9df7b1b82dd1dabdfc3
vzschool <vladislav.zakharov.school@gmail.com>
2022-04-11 13:38:42 -0700

    first commit

---
+++ app/src/androidTest/java/com/example/ad340_wk1/MainActivityTest.java
@@ -0,0 +1,29 @@
+package com.example.ad340_wk1;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void hasTextOnScreen() {
+        onView(withId(R.id.textView))
+                .check(matches(withText(R.string.name)));
+    }
+}
